875242b2097570564d900d4b206b9bca0a722d29
ralvescosta <rafael.rac.mg@gmail.com>
2020-06-21 12:45:21 -0300

    feat: ensure RemoteAddAccount and AccountModel

--- src/data/usercases/addAccount/remote.add.account.spec.ts
+++ src/data/usercases/addAccount/remote.add.account.spec.ts
@@ -4,12 +4,11 @@ import { RemoteAddAccount } from './remote.add.account'
 import { HttpStatusCode } from '~/data/protocols/http/http.post.client'
 import { MockHttpPostClient } from '~/data/__test__/mock.http'
 
-import { mockAddAccount } from '~/domain/__test__/mock.add.account'
+import { mockAddAccount, mockAccountModel } from '~/domain/__test__/mock.add.account'
 import { AddAccountParams } from '~/domain/usecases/addAccount'
 import { AccountModel } from '~/domain/models/account.models'
-// import { InvalidCredentialsError } from '~/domain/errors/invalid.credentials.error'
-// import { UnexpectedError } from '~/domain/errors/unexpected.error'
 import { EmailAlreadyExistError } from '~/domain/errors/email.already.exist.error'
+import { UnexpectedError } from '~/domain/errors/unexpected.error'
 
 // SUT = System under test
 type SutTypes = {
@@ -48,5 +47,35 @@ describe('', () => {
     }
     const promise = sut.add(mockAddAccount())
     await expect(promise).rejects.toThrow(new EmailAlreadyExistError())
+  })
+
+  test('Should throw UnexpectedError if HttpPostClint returns 400', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    }
+    const promise = sut.add(mockAddAccount())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('Should throw UnexpectedError if HttpPostClint returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.internalServerError
+    }
+    const promise = sut.add(mockAddAccount())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('Should return an AccountModel if HttpPostClient returns 200', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const httpResult = mockAccountModel()
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.success,
+      body: httpResult
+    }
+    const account = await sut.add(mockAddAccount())
+    expect(account).toEqual(httpResult)
   })
 })
