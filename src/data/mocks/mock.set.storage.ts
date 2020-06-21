d70f5ef2d87c89fc15ca1f520d8c59abeb16fcff
ralvescosta <rafael.rac.mg@gmail.com>
2020-06-20 22:35:21 -0300

    test: ensure local save access token throws if set storage throws

--- src/data/mocks/mock.set.storage.ts
+++ src/data/mocks/mock.set.storage.ts
@@ -1,8 +1,9 @@
 import { SetStorage } from '~/data/protocols/localStorage/set.storage'
 
-export class SetStorageSpy implements SetStorage {
+export class SetStorageMock implements SetStorage {
   public key: string
   public value: string
+
   async set (key: string, value: string): Promise<void> {
     this.key = key
     this.value = value
--- src/data/usercases/saveAccessToken/local.save.access.token.spec.ts
+++ src/data/usercases/saveAccessToken/local.save.access.token.spec.ts
@@ -1,27 +1,34 @@
 import faker from 'faker'
 import { LocalSaveAccessToken } from './local.save.access.token'
-import { SetStorageSpy } from '~/data/mocks/mock.set.storage'
+import { SetStorageMock } from '~/data/mocks/mock.set.storage'
 
 type SutType = {
-  setStorageSpy: SetStorageSpy
+  setStorageMock: SetStorageMock
   sut: LocalSaveAccessToken
 }
 
 function makeSut (): SutType {
-  const setStorageSpy = new SetStorageSpy()
-  const sut = new LocalSaveAccessToken(setStorageSpy)
+  const setStorageMock = new SetStorageMock()
+  const sut = new LocalSaveAccessToken(setStorageMock)
   return {
-    setStorageSpy,
+    setStorageMock,
     sut
   }
 }
 
 describe('LocalSaveAccessToken', () => {
   it('Should call SetStorage with correct value', async () => {
-    const { sut, setStorageSpy } = makeSut()
+    const { sut, setStorageMock } = makeSut()
     const accessToken = faker.random.uuid()
     await sut.save(accessToken)
-    expect(setStorageSpy.key).toBe('accessToken')
-    expect(setStorageSpy.value).toBe(accessToken)
+    expect(setStorageMock.key).toBe('accessToken')
+    expect(setStorageMock.value).toBe(accessToken)
+  })
+
+  it('Should throw if SetStorage throws', async () => {
+    const { sut, setStorageMock } = makeSut()
+    jest.spyOn(setStorageMock, 'set').mockRejectedValueOnce(new Error())
+    const promise = sut.save(faker.random.uuid())
+    await expect(promise).rejects.toThrow(new Error())
   })
 })
